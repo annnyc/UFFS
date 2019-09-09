0ab248e090b092024ea3005dc758e6c4c0e0ddf2
annnyc <31291673+annnyc@users.noreply.github.com>
2019-09-09 16:31:14 -0300

    Create kkkkkk.java

---
+++ GEX098/kkkkkk.java
@@ -0,0 +1,27 @@
+public abstract class pet{
+  String nome;
+  int idade;
+  public void brincar(){
+    System.out.println("oi");
+  }
+public abstract void emitirsom();
+
+}
+
+/////
+public class cachorro extends pet{
+  public void emitirsom(){
+    System.out.println("auauau");
+  }
+  public void cavar (){
+    System.out.println("grrrr");
+  }
+}
+///
+class trianguloequilatero extends triangulo{
+public float area(){
+  float resultado;
+  resultado = (getB()*getHS)
+}
+
+      }
