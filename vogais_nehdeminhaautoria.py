5bf7d2428708d2e7f6f64a46274f4f9d72aa553a
annnyc <31291673+annnyc@users.noreply.github.com>
2019-09-13 14:07:52 -0300

    Create vogais_nehdeminhaautoria.py

---
+++ vogais_nehdeminhaautoria.py
@@ -0,0 +1,6 @@
+x=input("Digite uma letra:\n")
+
+if x.lower=='a' or x.lower=='e' or x.lower=='i' or x.lower=='o' or x.lower=='u':
+    print("Essa letra é vogal!")
+else:
+    print("Essa letra é consoante!")
