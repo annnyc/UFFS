208e66523eed0527b3a69dfbf5fed02b07206629
annnyc <31291673+annnyc@users.noreply.github.com>
2019-09-13 14:08:05 -0300

    Delete vogais_nehdeminhaautoria.py

--- vogais_nehdeminhaautoria.py
+++
@@ -1,6 +0,0 @@
-x=input("Digite uma letra:\n")
-
-if x.lower=='a' or x.lower=='e' or x.lower=='i' or x.lower=='o' or x.lower=='u':
-    print("Essa letra é vogal!")
-else:
-    print("Essa letra é consoante!")
