e1674a639bca98a7b15c31f5df362695f6bbef08
annnyc <31291673+annnyc@users.noreply.github.com>
2019-12-17 17:51:10 -0300

    Add files via upload

---
+++ GEX605/prova1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct listasimplesmenteencadeada{
+  int conteudo;
+  struct listasimplesmenteencadeada *prev;
+  struct listasimplesmenteencadeada *next;
+}
+
+typedef struct listasimplesmenteencadeada simple;
+
+void insertlist(*)
